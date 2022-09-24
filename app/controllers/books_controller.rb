bee24305bb6140a617e495bd80032da9d921a7fc
Kurita <111416647+hinathu@users.noreply.github.com>
2022-09-24 16:20:52 +0000

    [Update] 編集画面にShow|Backのリンク先追加。更新した際に空白があった場合のバリデーション対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "Book was successfully created."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @books = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
